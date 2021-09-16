3a15ad5aed699fefd92f405ed9b688517f356354
Juan-Diego01 <82240991+Juan-Diego01@users.noreply.github.com>
2021-09-15 22:34:42 -0500

    Add files via upload

--- Formulario_PHP/FORMULARIO_PHP.php
+++ Formulario_PHP/FORMULARIO_PHP.php
@@ -1,4 +1,5 @@
 <?php
+//estas son las variables principales
 $nombre = $_POST['nombre'];
 $Snombre = $_POST['Snombre'];
 $apellido = $_POST['apellido'];
@@ -13,6 +14,7 @@ $Contacto = $_POST['Contacto'];
 $Mensaje = $_POST['Mensaje'];
 $fecha = $_POST['fecha'];
 
+//estas son las variables erróneas
 $nombreErr = $_POST['nombre'];
 $apellidoErr = $_POST['apellido'];
 $cargoErr = $_POST['cargo'];
@@ -20,6 +22,33 @@ $nombreEErr = $_POST['nombreE'];
 $SedePErr = $_POST['SedeP'];
 $ContactoErr = $_POST['contactoE'];
 $fechaErr = $_POST['fecha'];
+
+//estas son las cantidades posibles
+$cant1 = $_POST['cant1'];
+$cant2 = $_POST['cant2'];
+$cant3 = $_POST['cant3'];
+$cant4 = $_POST['cant4'];
+$cant5 = $_POST['cant5'];
+$cant6 = $_POST['cant6'];
+
+//Este es el total de los precios por la cantidad
+$totgans = $gans * $cant1;
+$tottort = $tort * $cant2;
+$totping = $ping * $cant3;
+$totpanart = $panart * $cant4;
+$totpanbl = $panbl * $cant5;
+$totpanint = $panint * $cant6;
+
+//estas son las variables de selección múltiple
+$gans = 2000;
+$tort = 5000;
+$ping = 1500;
+$panart = 4500;
+$panbl = 5000;
+$panint = 6000;
+$subtotal = $totgans + $tottort + $totping + $totpanart + $totpanbl + $totpanint;
+$desc = $subtotal * 0.2;
+$total = $subtotal - $desc;
 
 /*
 Dentro de la llave del anterior formulario se encuentra
@@ -40,65 +69,52 @@ echo "El número del personal en la empresa es: " . $Npersonal . "<br/>";
 echo "El contacto de la empresa es: " . $Contacto . "<br/>";
 echo "El mensaje dejado por el solicitando es: " . $Mensaje . "<br/>";
 echo "La fecha del día de la solicitud: " . $fecha . "<br/>";
-
-//Las siguientes son variables obligatorias completas
-
-$nombre = $apellido = $cargo = $nombreE = $SedeP = $Contacto = $fecha ="";
-
-//Las siguientes son variables obligatorias vacías
-
-$nombreErr = $apellidoErr = $cargoErr = $nombreEErr = $SedePErr = $ContactoErr = $fechaErr ="";
+echo "El subtotal de los productos comprados es:" . $subtotal . "<br/>";
 
 //Los siguientes son condicionales para los campos obligatorios:
-
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    if (empty($_POST['nombre'])) {
-        $nombreErr = "El nombre es requerido";
-    } else {
-        $nombre = test_input($_POST['nombre']);
+    if (!$_POST) {
+        header('Location:Formulario_HTML.html');
     }
 
-    if (empty($_POST['apellido'])) {
-        $apellidoErr = "El apellido es requerido";
-    } else {
-        $apellido = test_input($_POST['apellido']);
+    if (empty($_POST['nombre'])) {
+        echo "El nombre es requerido";
+    }
+
+    if (empty($_POST['apellido']) {
+        echo "El apellido es requerido";
     }
 
     if (empty($_POST['cargo'])) {
-        $cargoErr = "Su cargo es requerido";
-    } else {
-        $cargo = test_input($_POST['cargo']);
+        echo "Su cargo es requerido";
     }
 
-    if (empty($_POST['nombreE'])) {
-        $nombreEErr = "El nombre de la empresa es requerido";
-    } else {
-        $nombreE = test_input($_POST['nombreE']);
+    if (empty($_POST['nombreE']) {
+        echo "El nombre de la empresa es requerido";
     }
 
     if (empty($_POST['SedeP'])) {
-        $SedePErr = "La sede principal es requerida";
-    } else {
-        $SedeP = test_input($_POST['SedeP']);
+        echo "La sede principal es requerida";
     }
 
     if (empty($_POST['Contacto'])) {
-        $ContactoErr = "El email de contacto con la empresa es requerido";
-    } else {
-        $Contacto = test_input($_POST['Contacto']);
+        echo "El email de contacto con la empresa es requerido";
     }
 
     if (empty($_POST['fecha'])) {
-        $fechaErr = "La fecha es requerida";
+        echo "La fecha es requerida";
+    }
+
+//en esta parte se hacen los cálculos para poder determinar los descuentos.
+    if ($subtotal > 30000) {
+        $desc = $subtotal * 0.2;
+        echo "¡Felicidades!, el monto total es mayor a $30.000, te regalamos un descuento del 20%. En total tienes que pagar:" . $total . "<br/>"
+    } elseif {
+        ($subtotal = 30000) {
+            $desc = $subtotal * 0.1;
+            echo "¡Felicidades!, el monto total es igual a $30.000, te regalamos un descuento del 10%. En total tienes que pagar:" . $total . "<br/>"
     } else {
-        $fecha = test_input($_POST['fecha']);
+        ($subtotal <30000)
+        echo "Lo sentimos, tu monto de productos no alcanza los $30.000, por lo tanto no podemos ofrecerte el descuento del 15%."
     }
 }
-
-function test_input ($data) {
-    $data = trim($data); //elimina espacios en blanco y carácteres al principio y final de la cadena
-    $data = strimsplashes($data); //quita las barras de la cadena con comillas espaciadas
-    return $data; //devuelve el valor ingresado
-}
-
 ?>
